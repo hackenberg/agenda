ff47c7838fdb315c355d8320df0f5cc2ad6e5049
Lukas <lukas.hackenberg@gmx.at>
2013-02-02 19:12:25 +0100

    restructured Course, removed the unnecessary TreeSets and overrode toString

--- src/lhackenberg/agenda/Course.java
+++ src/lhackenberg/agenda/Course.java
@@ -1,24 +1,25 @@
 package lhackenberg.agenda;
 
-import org.joda.time.DateTime;
+public class Course {
 
-import java.io.Serializable;
-import java.sql.Connection;
-import java.util.TreeSet;
-
-public class Course implements Serializable {
-
+    private int _id;
     private String courseNr, name, semester;
-    private TreeSet<DateTime> assignments, lectures, tests;
 
-    protected Course(String courseNr, String name, String semester) {
+    protected Course(int _id, String courseNr, String name, String semester) {
+        set_id(_id);
         setCourseNr(courseNr);
         setName(name);
         setSemester(semester);
-        assignments = new TreeSet<DateTime>();
-        lectures = new TreeSet<DateTime>();
-        tests = new TreeSet<DateTime>();
     }
+
+    @Override
+    public String toString() {
+        return get_id() + " " + getCourseNr() + " " + getName() + " "
+                + getSemester();
+    }
+
+    protected int get_id() { return _id; }
+    protected void set_id(int _id) { this._id = _id; }
 
     protected String getCourseNr() { return this.courseNr; }
     protected void setCourseNr(String courseNr) { this.courseNr = courseNr; }
@@ -28,20 +29,5 @@ public class Course implements Serializable {
 
     protected String getSemester() { return this.semester; }
     protected void setSemester(String semester) { this.semester = semester; }
-
-    protected TreeSet<DateTime> getAssignments() { return assignments; }
-    protected void setAssignments(TreeSet<DateTime> assignments) {
-        this.assignments = assignments;
-    }
-
-    protected TreeSet<DateTime> getLectures() { return lectures; }
-    protected void setLectures(TreeSet<DateTime> lectures) {
-        this.lectures = lectures;
-    }
-
-    protected TreeSet<DateTime> getTests() { return tests; }
-    protected void setTests(TreeSet<DateTime> tests) {
-        this.tests = tests;
-    }
 
 } // of class Course
