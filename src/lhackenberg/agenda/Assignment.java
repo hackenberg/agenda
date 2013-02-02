8bb4676777be7aa43e0b0025cd8e7f1db77ebbef
Lukas <lukas.hackenberg@gmx.at>
2013-02-02 19:05:29 +0100

    restructured Assignment

--- src/lhackenberg/agenda/Assignment.java
+++ src/lhackenberg/agenda/Assignment.java
@@ -18,43 +18,19 @@ public class Assignment {
                 + getPts() + " " + getPts_max();
     }
 
-    protected int get_id() {
-        return _id;
-    }
+    protected int get_id() { return _id; }
+    protected void set_id(int _id) { this._id = _id; }
 
-    protected void set_id(int _id) {
-        this._id = _id;
-    }
+    protected int getCourse_id() { return course_id; }
+    protected void setCourse_id(int course_id) { this.course_id = course_id; }
 
-    protected int getCourse_id() {
-        return course_id;
-    }
+    protected int getDate_unix() { return date_unix; }
+    protected void setDate_unix(int date_unix) { this.date_unix = date_unix; }
 
-    protected void setCourse_id(int course_id) {
-        this.course_id = course_id;
-    }
+    protected int getPts() { return pts; }
+    protected void setPts(int pts) { this.pts = pts; }
 
-    protected int getDate_unix() {
-        return date_unix;
-    }
+    protected int getPts_max() { return pts_max; }
+    protected void setPts_max(int pts_max) { this.pts_max = pts_max; }
 
-    protected void setDate_unix(int date_unix) {
-        this.date_unix = date_unix;
-    }
-
-    protected int getPts() {
-        return pts;
-    }
-
-    protected void setPts(int pts) {
-        this.pts = pts;
-    }
-
-    protected int getPts_max() {
-        return pts_max;
-    }
-
-    protected void setPts_max(int pts_max) {
-        this.pts_max = pts_max;
-    }
-}
+} // of class Assignment
